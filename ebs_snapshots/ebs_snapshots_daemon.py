4b0012722ee1440dd2069917131227e6a0c08f38
Nathan Leiby <nathan.leiby@clever.com>
2015-01-14 17:22:42 -0800

    ebs_snapshots_daemon - update interval to 300s
    also remove unused check for backup_config failed to init.
    
    in general, it's OK if it fails to init because
    
        1. supports user creating local or s3 file after daemon started
           (since user may later remove/modify this file)
        2. warning logs as written to show the error is occuring

--- ebs_snapshots/ebs_snapshots_daemon.py
+++ ebs_snapshots/ebs_snapshots_daemon.py
@@ -8,10 +8,11 @@ import snapshot_manager
 from boto import ec2
 import kayvee
 
-aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID']
-aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
-aws_region=os.environ['AWS_REGION']
-config_path=os.environ['BACKUP_CONFIG']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+aws_region = os.environ['AWS_REGION']
+config_path = os.environ['BACKUP_CONFIG']
+
 
 def get_backup_conf(path):
     """ Gets backup config from file or S3 """
@@ -20,21 +21,22 @@ def get_backup_conf(path):
     else:
         return FileBackupConfig(path)
 
+
 def create_snapshots(backup_conf):
-    ec2_connection = ec2.connect_to_region(aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    ec2_connection = ec2.connect_to_region(
+        aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
     for volume, params in backup_conf.get().iteritems():
         print kayvee.formatLog("ebs-snapshots", "info", "about to take ebs snapshot {} - {}".format(volume, params))
         interval = params.get('interval', 'daily')
         max_snapshots = params.get('max_snapshots', 0)
-        snapshot_manager.run(ec2_connection, volume, interval, max_snapshots) 
+        snapshot_manager.run(ec2_connection, volume, interval, max_snapshots)
+
 
 class EbsSnapshotsDaemon(Daemon):
 
-    def run(self, interval=2):
+    def run(self, interval=300):
         backup_conf = get_backup_conf(config_path)
-        if not backup_conf:
-            raise Exception("Backup config failed to initialize")
-        
+
         while True:
-            create_snapshots(backup_conf) 
+            create_snapshots(backup_conf)
             time.sleep(interval)
